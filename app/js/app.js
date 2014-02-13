9e39fd4dbcf941e76658504989d2dbe52848436d
Alexander Selzer <aselzer3@gmail.com>
2014-02-13 15:10:37 +0100

    Added word count.

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,17 @@
-// Nothing
+var chemistryAB = angular.module("chemistryAB", []);
+
+chemistryAB.controller("bibliography", function($scope, $http) {
+  $http.get("citations.json").success(function(citations) {
+    $scope.references = citations;
+  });
+})
+
+.directive("document", function() {
+  return {
+    link: function(scope, document, attribute) {
+      Countable.live(document[0], function(count) {
+        console.log("Word count:", count);
+      });
+    }
+  }
+});
